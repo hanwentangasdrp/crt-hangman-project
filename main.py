123ccc3fc8292cb6a0d76442af64e10783def2c7
megansasdrp <megan.shaw@students.asdrp.org>
2026-07-09 17:06:46 -0700

    Fix word list definition and add play function

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import random
 
 print("Let's play Hangman!")
 
-words = ["python", "apple", "computer", "banana"]
+def get_words = ["python", "apple", "computer", "banana"]
 
+def play(word):
+  word_completion = "_" ' len(word)
+  guessed = False
+  guessed_letters = []
+  guessed_words = []
+  tries = 6 
