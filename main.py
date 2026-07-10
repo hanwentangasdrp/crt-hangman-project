c83a9581fb4fe03e67ac136c7c0849f47a774b59
Langyyyyy <htygr2010@gmail.com>
2026-07-09 20:00:40 -0400

    added display

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ print("Let's play Hangman!")
 
 words = ["python", "apple", "computer", "banana"]
 
+answer = random.choice(words)
+
+guessed_letters = []
+
+lives = 6
+
+# this should show the display of the word
+def show_word():
+    display = ""
+    for letter in answer:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+    return display
+
+print(show_word())
+print("Lives left:", lives)
